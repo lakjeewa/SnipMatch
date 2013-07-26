c64054e779af2aef8441d384324ec23f47675802
lakjeewa <lakjeewadmm@gmail.com>
2013-07-26 23:46:14 +0530

    Add search type handling functionality.

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/handlers/CommandHandler.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/handlers/CommandHandler.java
@@ -1,11 +1,16 @@
 package org.eclipse.recommenders.snipmatch.handlers;
 
+import java.util.ArrayList;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
+import org.eclipse.recommenders.snipmatch.core.Snippet;
+import org.eclipse.recommenders.snipmatch.core.TemplateProcessor;
 import org.eclipse.recommenders.snipmatch.rcp.UserEnvironment;
 import org.eclipse.recommenders.snipmatch.search.SearchBox;
+import org.eclipse.recommenders.snipmatch.search.SnipMatchSearchEngine;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 
@@ -16,6 +21,12 @@ import org.eclipse.ui.PlatformUI;
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class CommandHandler extends AbstractHandler {
+
+	private String lastQuery = null;
+	private ArrayList<Snippet> searchResult = null;
+	private SnipMatchSearchEngine snipMatchSearchEngine;
+	private SearchBox searchBox = null;
+
 	/**
 	 * The constructor.
 	 */
@@ -29,13 +40,44 @@ public class CommandHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		
+
 		if (editor instanceof JavaEditor) {
 			UserEnvironment env = new UserEnvironment(editor);
-			SearchBox searchBox = new SearchBox(env);
+			searchBox = new SearchBox(env, this);
 			searchBox.show();
+			snipMatchSearchEngine = new SnipMatchSearchEngine();
 		}
 
 		return null;
+	}
+
+	/**
+	 * This method handle the type action on the search query text box
+	 * 
+	 * @param query
+	 *            search query
+	 */
+	public void handleTyping(String query) {
+
+		if (lastQuery != null && lastQuery.trim().equals(query.trim())) {
+			return;
+		} else {
+			lastQuery = query;
+			searchResult = snipMatchSearchEngine.search(query);
+			searchBox.displayResults(searchResult);
+		}
+	}
+
+	/**
+	 * This method insert the code snippet into the editor
+	 * 
+	 * @param resultIndex
+	 *            Index of selected snippet
+	 */
+	public void insertResult(int resultIndex) {
+		Snippet snippet = searchResult.get(resultIndex);
+		System.out.println(snippet.getCode());
+		TemplateProcessor templateProcessor = new TemplateProcessor();
+		templateProcessor.insertTemplate(snippet);
 	}
 }
