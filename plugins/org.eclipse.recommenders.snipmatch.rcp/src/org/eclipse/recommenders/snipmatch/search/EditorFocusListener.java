6fc1e63249594afd011ccf6171393ce108384916
lakjeewa <lakjeewadmm@gmail.com>
2013-08-06 16:49:57 +0530

    Focus issues.

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/search/EditorFocusListener.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/search/EditorFocusListener.java
@@ -1,9 +1,10 @@
 package org.eclipse.recommenders.snipmatch.search;
 
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.swt.widgets.Widget;
 
 /**
  * This Listener dispose the shell when a focus event take place on the active
@@ -20,8 +21,18 @@ public class EditorFocusListener implements Listener {
 
 	@Override
 	public void handleEvent(Event event) {
-		if (shell != null && event.widget.equals(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()))
-			shell.dispose();
+		// if (shell != null &&
+		// event.widget.equals(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()))
+		// shell.dispose();
+		if (shell != null) {
+			Widget widget = event.widget;
+			for (Control child : shell.getChildren()) {
+				if (child == widget)
+					return;
+			}
+		}
+		shell.dispose();
+
 	}
 
 }
