b61093c934f540d5c41606983c5c92a16ddb9691
lakjeewa <lakjeewadmm@gmail.com>
2013-08-11 14:25:34 +0530

    Change variable resolution.

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/core/TemplateProcessor.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/core/TemplateProcessor.java
@@ -1,21 +1,9 @@
 package org.eclipse.recommenders.snipmatch.core;
 
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.internal.corext.template.java.ImportsResolver;
-import org.eclipse.jdt.internal.corext.template.java.JavaContext;
-import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
-import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
-import org.eclipse.jdt.internal.ui.text.template.contentassist.TemplateProposal;
-import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextOperationTarget;
-import org.eclipse.jface.text.Position;
-import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.text.templates.GlobalTemplateVariables;
 import org.eclipse.jface.text.templates.Template;
-import org.eclipse.jface.text.templates.TemplateContext;
-import org.eclipse.jface.text.templates.TemplateContextType;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 
@@ -26,8 +14,6 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
  * 
  */
 public class TemplateProcessor {
-
-	private JavaEditor editor;
 
 	/**
 	 * Insert the selected snippet into editor
@@ -43,34 +29,13 @@ public class TemplateProcessor {
 
 		ISourceViewer sourceViewer = (ISourceViewer) activeEditor.getAdapter(ITextOperationTarget.class);
 
-		Point range = sourceViewer.getSelectedRange();
+		Template template = new Template("name", "description", "java-statements", snippet.getCode(), true);
 
-		Template template = new Template("", "", "org.eclipse.recommenders.snipmatch.rcp.javaContextType", snippet.getCode(), true);
-		IRegion region = new Region(range.x, range.y);
+		IDocument doc = activeEditor.getDocumentProvider().getDocument(activeEditor.getEditorInput());
+		SnipMatchJavaTemplatesPage page = new SnipMatchJavaTemplatesPage(activeEditor, sourceViewer);
+		page.insertTemplate(template, doc);
+		page.dispose();
 
-		// TemplateVariableResolver resolver = new TemplateVariableResolver();
-
-		TemplateContextType contextType = new TemplateContextType("org.eclipse.recommenders.snipmatch.rcp.javaContextType");
-		// TemplateContextType contextType = new JavaContextType();
-		contextType.addResolver(new GlobalTemplateVariables.Cursor());
-		contextType.addResolver(new GlobalTemplateVariables.WordSelection());
-		contextType.addResolver(new GlobalTemplateVariables.Date());
-		contextType.addResolver(new GlobalTemplateVariables.Dollar());
-		contextType.addResolver(new GlobalTemplateVariables.LineSelection());
-		contextType.addResolver(new GlobalTemplateVariables.Time());
-		contextType.addResolver(new GlobalTemplateVariables.User());
-		contextType.addResolver(new GlobalTemplateVariables.Year());
-		contextType.addResolver(new ImportsResolver("import", "Import Statement"));
-
-		ICompilationUnit cu = (ICompilationUnit) EditorUtility.getEditorInputJavaElement(activeEditor, false);
-		Position p = new Position(range.x, range.y);
-
-		TemplateContext ctx = new JavaContext(contextType, sourceViewer.getDocument(), p, cu);
-
-		//TemplateContext ctx = new DocumentTemplateContext(contextType, sourceViewer.getDocument(), range.x, range.y);
-
-		TemplateProposal proposal = new TemplateProposal(template, ctx, region, null);
-		proposal.apply(sourceViewer, (char) 0, 0, 0);
 	}
 
 }
